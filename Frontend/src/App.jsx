62d2715fb3b5c4cdd58b64063316bdf9a608018b
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-09-04 21:39:38 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,11 +7,12 @@ import { Todos } from './components/Todos'
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos")
-  .then(async (res) =>{
-    const json = await res.json();
-    setTodos(json.todos);
-  });
+ useEffect(()=>{ fetch("http://localhost:3000/todos")
+      .then(async (res) =>{
+      const json = await res.json();
+      setTodos(json.todos);
+      })
+  },[])
   
 
 
